c61c89307db7a30b0f81b62b48eb2ef6d0c0a568
davidetaribelli <davide.taribelli@gmail.com>
2023-05-26 10:47:18 +0200

    refactor

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,12 +10,16 @@ export default {
         }
     },
     methods: {
-        getFlag(lang) {
-            if (lang == 'en') {
-                return 'fi fi-gb'
+        getFlag(language) {
+            if (language == 'en') {
+                return 'fi fi-gb';
             }
-            return 'fi fi-' + lang
+            return 'fi fi-' + language;
         },
+        getPoster(img) {
+            let urlPath = "https://image.tmdb.org/t/p/w342/" + img;
+            return urlPath;
+        }
 
     }
 
@@ -26,16 +30,26 @@ export default {
     <main>
         <div class="films">
             <div class="film" v-for="element, i in store.film">
-                <h3>Titolo:{{ element.title }}</h3>
-                <h4>Titolo originale:{{ element.original_title }}</h4>
-                <h5 :class="getFlag(element.original_language)"></h5>
-                <h5>Voto:{{ element.vote_average }}</h5>
+                <img v-show="(element.backdrop_path != null)" :src="getPoster(element.backdrop_path)" :alt="element.title">
+                <p v-show="(element.backdrop_path == null)">IMAGE NON EXIST</p>
+                <div class="info">
+                    <h4>Titolo:{{ element.title }}</h4>
+                    <h5>Titolo originale:{{ element.original_title }}</h5>
+                    <h6 :class="getFlag(element.original_language)"></h6>
+                    <h4>Voto: <i class="fa-solid fa-star"></i></h4>
+                    <small>Overview: {{ element.overview.slice(0, 150) }}[...]</small>
+                </div>
             </div>
             <div class="serie" v-for="element, i in store.serieTv">
-                <h3>Titolo:{{ element.name }}</h3>
-                <h4>Titolo originale:{{ element.original_name }}</h4>
-                <h5 :class="getFlag(element.original_language)"></h5>
-                <h5>Voto:{{ element.vote_average }}</h5>
+                <img v-show="(element.backdrop_path != null)" :src="getPoster(element.backdrop_path)" :alt="element.name">
+                <p v-show="(element.backdrop_path == null)">IMAGE NON EXIST</p>
+                <div class="info">
+                    <h3>Titolo:{{ element.name }}</h3>
+                    <h5>Titolo originale:{{ element.original_name }}</h5>
+                    <h6 :class="getFlag(element.original_language)"></h6>
+                    <h4>Voto: <i class="fa-solid fa-star"></i></h4>
+                    <small>{{ element.overview }}</small>
+                </div>
             </div>
         </div>
 
@@ -44,6 +58,7 @@ export default {
 
 <style scoped lang="scss">
 @use '../style/general.scss' as *;
+
 
 main {
     text-align: center;
@@ -55,12 +70,42 @@ main {
         gap: 1em;
         width: 100%;
 
+
+
         .film,
         .serie {
-            width: calc(100% / 8);
-            min-height: 300px;
-            background-color: blue;
+            @include flex(row, center, center, wrap);
+            width: calc(100% / 6);
+            height: 350px;
+            color: white;
+
+            &:hover img {
+                display: none;
+            }
+
+            &:hover p {
+                display: none;
+            }
+
+            &:hover .info {
+                display: block;
+            }
         }
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: top;
+        }
+
+        .info {
+            display: flex;
+            justify-content: flex-start;
+            display: none;
+            overflow-y: scroll;
+        }
+
     }
 }
 </style>
